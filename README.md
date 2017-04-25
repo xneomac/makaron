143cc8c674873935bb763ec228d868d2a4f50274
nmartignoni <noel.martignoni@easymov.fr>
2017-04-25 22:13:55 +0200

    add README, script and setup.py from old project

---
+++ README.md
@@ -0,0 +1,26 @@
+# makaron
+
+A simple way to handle your version number
+Put a variable `__version__` in the `setup.py` file.
+
+## version
+
+0.0.1
+
+## Quickstart
+
+Makaron follow this version model:
+
+    major.minor.patch
+
+Just type the name of the number you want to increase:
+
+    makaron major
+
+or
+
+    makaron minor
+
+or
+
+    makaron patch
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+__version__ = '0.0.1'
+
+setup(
+    name='makaron',
+    version=__version__,
+    description='Version handler',
+    author='Noel Martignoni',
+    author_email='noel@martignoni.fr',
+    url='https://gitlab.com/makaron/makaron',
+    scripts = ['scripts/makaron'],
+    install_requires=[],
+    packages = [],
+)
